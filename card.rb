84b4fb4df0ce47292e32dc5f4914646c8c616ffe
11albina19 <11albina19@gmail.com>
2023-12-13 15:06:33 +0400

    version 0.3

--- card.rb
+++ card.rb
@@ -4,4 +4,10 @@ class Card
 
   attr_accessor :suit, :rank, :points #масть, достоинство, очки
 
+  def initialize(suit, rank, points)
+    #!Переделывать ли на перечисление, если все равно будет создаваться программно?
+    @suit   = suit
+    @rank   = rank
+    @points = points
+  end
 end
