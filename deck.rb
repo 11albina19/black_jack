387d0fcfd98934dcee20b1212c5963e203bfa292
11albina19 <11albina19@gmail.com>
2023-12-15 18:18:51 +0400

    version 0.6

--- deck.rb
+++ deck.rb
@@ -6,7 +6,23 @@ class Deck
 
   end
 
+  def take_card
+    regenerate_cards if cards.empty?
+    cards.random_pop
+  end
+  
   private
   attr_reader :cards
+
+  def regenerate_cards
+
+  end
+  def random_pop
+
+  end
+
+  def delete
+
+  end
 
 end
