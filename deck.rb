b05add35f4ad590e55ba7c736686dfd01ddecdbe
11albina19 <11albina19@gmail.com>
2023-12-15 18:28:08 +0400

    version 0.7

--- deck.rb
+++ deck.rb
@@ -3,22 +3,29 @@
 class Deck
 
   def initialize()
-
+    regenerate_cards
   end
 
   def take_card
     regenerate_cards if cards.empty?
     cards.random_pop
   end
-  
+
   private
   attr_reader :cards
 
   def regenerate_cards
+    c1 = Card.new('Черви', 'Десятка', 10)
+    cards << c1
 
+    c2 = Card.new('Черви', 'Дама', 10)
+    cards << c2
+
+    c3 = Card.new('Пики', 'Тройка', 3)
+    cards << c3
   end
   def random_pop
-
+    card = cards.sample
   end
 
   def delete
