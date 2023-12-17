b92d30ed717a802bd7241bdc25e9b2ffe73209d3
11albina19 <11albina19@gmail.com>
2023-12-17 16:36:21 +0400

    version 0.17

--- game.rb
+++ game.rb
@@ -8,4 +8,13 @@ require_relative 'mechanics'
 require_relative 'menu'
 
 class Game
+
+  MENU = [
+    {number: 1, message: 'Пропустить', action: :skip },
+    {number: 2, message: 'Добавить карту', action: :add },
+    {number: 3, message: 'Открыть карты', action: :open },
+    {number: 99, message: 'Завершить выполнение программы', action: :show_menu }
+  ].freeze
+
+
 end
