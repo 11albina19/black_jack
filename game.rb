49100512643308ed533983ca881af9b580bed8f9
11albina19 <11albina19@gmail.com>
2023-12-18 02:30:30 +0400

    version 3.2

--- game.rb
+++ game.rb
@@ -18,7 +18,25 @@ class Game
   end
 
   def determine(player, dealer)
-
+    number_player = player.points
+    number_dealer = dealer.points
+    champion = 0
+    if number_dealer > 21 && number_player > 21
+      champion = 3 #ничья
+    elsif number_dealer > 21
+      champion = 1 #у дилера > 21, победитель игрок
+    elsif number_player > 21
+      champion = 2 #у игрока > 21, победитель дилер
+    elsif number_player == number_dealer
+      champion = 3 #ничья
+    elsif number_player > number_dealer
+      champion = 1 #игрок набрал больше очков, победитель игрок
+    elsif number_dealer > number_player
+      champion = 2 #дилер набрал больше очков, победитель дилер
+    else
+      champion = 4 #что-то странное
+    end
+    champion
   end
 
   def check(player, dealer)
