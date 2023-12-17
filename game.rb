f2663aae7d590287a7e93e70041049a0de64b1ba
11albina19 <11albina19@gmail.com>
2023-12-17 16:26:46 +0400

    version 0.16

--- game.rb
+++ game.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
+require_relative 'card'
+require_relative 'deck'
+require_relative 'participant'
+require_relative 'player'
+require_relative 'dealer'
+require_relative 'mechanics'
+require_relative 'menu'
 
 class Game
 end
